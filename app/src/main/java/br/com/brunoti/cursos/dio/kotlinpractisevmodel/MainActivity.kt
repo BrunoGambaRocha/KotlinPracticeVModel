a12475b22aae92468534e3180b2e41d715c168ad
BrunoGambaRocha <bruno.roch@gmail.com>
2021-08-27 17:59:43 -0300

    build app ui

---
+++ app/src/main/java/br/com/brunoti/cursos/dio/kotlinpractisevmodel/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.brunoti.cursos.dio.kotlinpractisevmodel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
