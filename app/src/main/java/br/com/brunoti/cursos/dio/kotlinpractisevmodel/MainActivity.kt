de6e61cdd56954aee0681db58a618f1982a2e131
BrunoGambaRocha <bruno.roch@gmail.com>
2021-08-27 18:09:06 -0300

    add testing lifecycle functions

--- app/src/main/java/br/com/brunoti/cursos/dio/kotlinpractisevmodel/MainActivity.kt
+++ app/src/main/java/br/com/brunoti/cursos/dio/kotlinpractisevmodel/MainActivity.kt
@@ -2,9 +2,11 @@ package br.com.brunoti.cursos.dio.kotlinpractisevmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,9 +19,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        logar(valor = "onCreate")
+
         initDados()
         initContador()
         initClick()
+    }
+
+    override fun onStart() {
+        logar(valor = "onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        logar(valor = "onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        logar(valor = "onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        logar(valor = "onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        logar(valor = "onDestroy")
+        super.onDestroy()
+    }
+
+    private fun logar(tag: String = "Ciclo de Vida", valor: String){
+        Log.d(tag, valor)
     }
 
     private fun initClick() {
